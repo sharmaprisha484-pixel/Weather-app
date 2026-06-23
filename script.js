c40c3b5828800287c65ccf08d80a2f428a630475
Prisha Sharma <sharmaprisha484@gmail.com>
2026-06-23 18:34:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,74 @@
-async function test(){
-    let city=document.getElementById("city").value;
-    console.log(city);
+window.onload = function () {
+    let lastCity = localStorage.getItem("lastCity");
 
-    let url = `https://wttr.in/${city}?format=j1`;
-    console.log(url);
+    if (lastCity) {
+        document.getElementById("city").value = lastCity;
+        test(); // last searched city automatically load ho jayegi
+    }
+}
 
-    let response = await fetch(url);
-    console.log(response);
 
-    let data = await response.json();
-    console.log(data);
-    
-    console.log(data.current_condition[0]);
-    
-    let temp = data.current_condition[0].temp_C;
-    let humidity = data.current_condition[0].humidity;
-    let condition = data.current_condition[0].weatherDesc[0].value;
+async function test() {
 
-    console.log(temp);
-    console.log(humidity);
-    console.log(condition);
+    try {
 
-    document.getElementById("temp").textContent = temp + "℃";
-    document.getElementById("condition").textContent = condition;
-    document.getElementById("humidity").textContent = humidity + "%";
-    document.getElementById("cityName").textContent = city;
+        let city = document.getElementById("city").value;
+        console.log(city);
 
-    let wind = data.current_condition[0].windspeedKmph;
-    document.getElementById("wind").textContent = wind + "Km/h";
+        if (city === "") {
+            alert("Please enter a city name.");
+            return;
+        }
 
-    let feels = data.current_condition[0].FeelsLikeC;
-    document.getElementById("feels").textContent = feels + "℃";
+        let url = `https://wttr.in/${city}?format=j1`;
+        console.log(url);
 
-    let icon = data.current_condition[0].weatherIconUr1[0].value;
-    document.getElementById("icon").src = icon;
-    
-    
-    
-     
+        document.getElementById("loading").textContent = "Loading...";
+
+        let response = await fetch(url);
+        console.log(response);
+
+        let data = await response.json();
+        console.log(data);
+
+        document.getElementById("loading").textContent = "";
+
+        console.log(data.current_condition[0]);
+
+        let temp = data.current_condition[0].temp_C;
+        let humidity = data.current_condition[0].humidity;
+        let condition = data.current_condition[0].weatherDesc[0].value;
+
+        console.log(temp);
+        console.log(humidity);
+        console.log(condition);
+
+        document.getElementById("temp").textContent = temp + "℃";
+        document.getElementById("condition").textContent = condition;
+        document.getElementById("humidity").textContent = humidity + "%";
+        document.getElementById("cityName").textContent = city;
+
+        let wind = data.current_condition[0].windspeedKmph;
+        document.getElementById("wind").textContent = wind + " Km/h";
+
+        let feels = data.current_condition[0].FeelsLikeC;
+        document.getElementById("feels").textContent = feels + "℃";
+
+        let icon = data.current_condition[0].weatherIconUrl[0].value;
+        document.getElementById("icon").src = icon;
+
+        let today = new Date();
+        document.getElementById("date").textContent =
+            today.toDateString();
+
+        localStorage.setItem("lastCity", city);
+
+    }
+
+    catch (error) {
+        console.error(error);
+        document.getElementById("loading").textContent = "";
+        alert("Something went wrong!");
+    }
+
 }
